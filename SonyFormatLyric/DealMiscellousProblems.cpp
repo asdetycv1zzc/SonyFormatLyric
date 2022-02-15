5d2ede99bf88e1699b1fb8482433c69f08eab3bd
张 哲城 <junguan_quanli@outlook.com>
2022-02-15 17:48:52 +0800

    Issue Fixed

---
+++ SonyFormatLyric/DealMiscellousProblems.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string>
+#include<cstring>
+#include<Windows.h>
+
+std::wstring stringToWstring(const std::string& str)
+{
+	LPCSTR pszSrc = str.c_str();
+	int nLen = MultiByteToWideChar(CP_ACP, 0, pszSrc, -1, NULL, 0);
+	if (nLen == 0)
+		return std::wstring(L"");
+
+	wchar_t* pwszDst = new wchar_t[nLen];
+	if (!pwszDst)
+		return std::wstring(L"");
+
+	MultiByteToWideChar(CP_ACP, 0, pszSrc, -1, pwszDst, nLen);
+	std::wstring wstr(pwszDst);
+	delete[] pwszDst;
+	pwszDst = NULL;
+
+	return wstr;
+}
+/*https ://blog.csdn.net/qicaiyuwu/article/details/89346871*/
---
+++ SonyFormatLyric/DealMiscellousProblems.h
@@ -0,0 +1,6 @@
+#pragma once
+#include<iostream>
+#include<string>
+#include<Windows.h>
+
+std::wstring stringToWstring(const std::string& str);
